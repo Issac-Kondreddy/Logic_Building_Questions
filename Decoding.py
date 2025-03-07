d7d467bc637325278b72ea3c4f0cf71c1b858ab7
issac-kondreddy <ixk93660@ucmo.edu>
2025-03-07 13:25:18 -0600

    Possible Decoding

--- Decoding.py
+++ Decoding.py
@@ -1,26 +1,16 @@
-digit_to_char = {str(i):chr(64+i) for i in range(1,27)}
+digits_to_char = {str(i):chr(64+i) for i in range(1, 27)}
+
 def decode(digits):
     if not digits:
-        # If there are no more digits left, return a list with an empty string
         return [""]
-
     results = []
-
-    # Check if the first digit is valid
-    if digits[0] in digit_to_char:
-        # Get all combinations starting with this one digit
-        for combination in decode(digits[1:]):
-            results.append(digit_to_char[digits[0]] + combination)
-
-    # Check if the first two digits form a valid number
-    if len(digits) >= 2 and digits[:2] in digit_to_char:
-        # Get all combinations starting with these two digits
-        for combination in decode(digits[2:]):
-            results.append(digit_to_char[digits[:2]] + combination)
-
+    if digits[0] in digits_to_char:
+        for i in decode(digits[1:]):
+            results.append(digits_to_char[digits[0]] + i)
+    if len(digits)>=2 and digits[:2] in digits_to_char:
+        for i in decode(digits[2:]):
+            results.append(digits_to_char[digits[:2]] + i)
     return results
 
-# Example usage
-encoded_string = "131"
-decoded_strings = decode(encoded_string)
-print(decoded_strings)
+seq = "12345"
+print(decode(seq))
